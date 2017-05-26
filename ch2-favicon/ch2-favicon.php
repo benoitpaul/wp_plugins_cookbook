fa715fa9e753220cacdad348c43b12d2f004b584
Benoit Paul <benoit.paul@barco.com>
2017-05-26 15:11:18 -0400

    chapter 2 in progress

---
+++ ch2-favicon/ch2-favicon.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Chapter 2 - Favicon
+Plugin URI:
+Description: Declares a plugin that will be visible in the WordPress admin interface
+Version: 1.0
+Author: Yannick Lefebvre
+Author URI: http://ylefebvre.ca
+License: GPLv2
+*/
+
+add_action( 'wp_head', 'ch2fi_page_header_output' );
+function ch2fi_page_header_output() {
+    $icon_url = plugins_url( 'favicon.ico', __FILE__ );
+    ?>
+        <link rel="shortcut icon" href="<?php echo $icon_url; ?>" type="image/x-icon" />
+    <?php 
+}
+?>
