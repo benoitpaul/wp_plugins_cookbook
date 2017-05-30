17b1a70ba8db40b02a937d7b5455862c5315f5d6
Benoit Paul <benoit.paul@barco.com>
2017-05-30 07:29:55 -0400

    chapter 3 options

---
+++ ch3-individual-options/ch3-individual-options.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Chapter 3 – Individual Options
+Plugin URI:
+Description: Declares a plugin that will be visible in the WordPress admin interface
+Version: 1.0
+Author: Yannick Lefebvre
+Author URI: http://ylefebvre.ca
+License: GPLv2
+*/
+
+register_activation_hook( __FILE__, 'ch3io_set_default_options' );
+function ch3io_set_default_options() {
+    if ( get_option( 'ch3io_version' ) === false ) {
+        add_option( 'ch3io_ga_account_name', 'UA-000000-0' );
+        add_option( 'ch3io_track_outgoing_links', 'false' );
+        add_option( 'ch3io_version', '1.1' );
+    } elseif ( get_option( 'ch3io_version' ) < 1.1 ) {
+        add_option( 'ch3io_track_outgoing_links', 'false' );
+        update_option( 'ch3io_version', '1.1' );
+    }
+}
+?>
